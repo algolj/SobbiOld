3fdbe9e3a66ba240df008ba988025d6745658ac0
VaniaToper <vanjaqwe58@gmail.com>
2022-01-20 11:34:19 +0100

    feat🔨: add main button

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from 'react';
 import s from './Home.module.scss';
+import Button from '../../components/UI/Button/Button';
 
 const Home: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button padding={'30px'} children={'Create'} />
+    </div>
+  );
 };
 
 export default Home;
