bd49d9ca517fbaa90d35d41b9e8a8f984358c2bd
VaniaToper <vanjaqwe58@gmail.com>
2022-01-24 00:25:44 +0100

    feat🔨: add chat switcher

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import style from './Chat.module.scss';
 import SearchInput from '../../pages/inputs/SearchInput/SearchInput';
 import sprite from '../../assets/sprite.svg';
 import Message from '../Message/Message';
-
-const Chat = () => {
+interface IProps {
+  isHide: boolean;
+}
+const Chat: FC<IProps> = ({ isHide }) => {
   return (
-    <div className={style.chat}>
+    <div className={isHide ? style.chat : style.chat_hide}>
       <div className={style.chat__messages}>
         <Message isReceiving={false} />
         <Message isReceiving={true} />
--- frontend/src/pages/Room/Room.tsx
+++ frontend/src/pages/Room/Room.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Room.module.scss';
 import NavBar from '../../components/NavBar/NavBar';
 import TestsSearcher from '../../components/TestsSeacher/TestsSearcher';
 import Chat from '../../components/Chat/Chat';
+import Button from '../../components/UI/Button/Button';
 
 const Room = () => {
+  const [isVideo, setIsVideo] = useState<boolean>(true);
   return (
     <div className={style.room}>
       <div className={style.room__chats}>
-        {/*<img*/}
-        {/*  className={style.room__image}*/}
-        {/*  src={'./assets/icon/video1.jpg'}*/}
-        {/*  alt=""*/}
-        {/*/>*/}
-        <Chat />
+        <div className={style.room__chats_switcher}>
+          <Button onClick={() => setIsVideo(false)}>Chat</Button>
+          <Button onClick={() => setIsVideo(true)}>Video</Button>
+        </div>
+        <img
+          className={isVideo ? style.room__video : style.room__video_hide}
+          src={'./assets/icon/video1.jpg'}
+          alt=""
+        />
+        <Chat isHide={!isVideo} />
       </div>
       <div className={style.room__menu}>
         <div className={style.room__navigation}>
