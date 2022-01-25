71aedc97f7cfb8de2a3d5af8fc84a1dc11a18480
VaniaToper <vanjaqwe58@gmail.com>
2022-01-26 00:17:01 +0100

    feat🔨: add burger

--- frontend/src/components/UI/Button/Button.tsx
+++ frontend/src/components/UI/Button/Button.tsx
@@ -4,14 +4,21 @@ import style from './Button.module.scss';
 interface IProps {
   children?: React.ReactChild;
   onClick?: () => void;
+  active?: boolean;
   props?: any;
 
   [key: string]: any;
 }
 
-const Button: FC<IProps> = ({ children, onClick, ...props }) => {
+const Button: FC<IProps> = ({ children, onClick, active, ...props }) => {
   return (
-    <button {...props} className={style.button} onClick={onClick}>
+    <button
+      {...props}
+      className={
+        active ? `${style.button} ${style.button_active}` : style.button
+      }
+      onClick={onClick}
+    >
       {children}
     </button>
   );
