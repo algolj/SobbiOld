2c9f6885da7aa1e2ae295ed062b470c53073aeff
VaniaToper <vanjaqwe58@gmail.com>
2022-01-26 23:36:26 +0100

    feat🔨: add modal

--- frontend/src/pages/inputs/FormInput/FormInput.tsx
+++ frontend/src/pages/inputs/FormInput/FormInput.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { FC } from 'react';
+import style from './FormInput.module.scss';
 
-const FormInput = () => {
-  return <div></div>;
+interface IProps {
+  label: string;
+  type?: string;
+}
+
+const FormInput: FC<IProps> = ({ label, type = 'text' }) => {
+  return (
+    <label className={style.input__wrapper} htmlFor="fromInput">
+      <input className={style.input} id={'fromInput'} type={type} />
+      <span className={style.input__label}>{label}</span>
+    </label>
+  );
 };
 
 export default FormInput;
